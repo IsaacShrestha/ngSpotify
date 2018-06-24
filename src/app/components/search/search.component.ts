48e5244b5bec9be77ac30caff195d05af422efcc
IsaacShrestha <ishrestha@unomaha.edu>
2018-06-24 13:40:07 -0500

    artists display not working- auth required

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../../_services/spotify.service';
+import { Artist } from '../../_models/Artist';
 
 @Component({
   selector: 'app-search',
@@ -8,6 +9,7 @@ import { SpotifyService } from '../../_services/spotify.service';
 })
 export class SearchComponent implements OnInit {
   searchStr: string;
+  searchRes: Artist[];
   constructor(private _spotifyService:SpotifyService) {
 
    }
@@ -17,7 +19,8 @@ export class SearchComponent implements OnInit {
 
   searchMusic() {
     this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
-      console.log(res.artists.items);
+      //console.log(res.artists.items);
+      this.searchRes = res.artists.items;
     });
   }
 
